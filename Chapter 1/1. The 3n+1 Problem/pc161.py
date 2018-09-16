1cd23d2ef27350ecdd2c5f8ae95f671da88ab242
Chris <cjpileggi@gmail.com>
2018-09-16 12:37:00 -0400

    Updated 3n+1 Problem to tailor to the online judge

--- Chapter 1/1. The 3n+1 Problem/pc161.py
+++ Chapter 1/1. The 3n+1 Problem/pc161.py
@@ -1,23 +1,113 @@
 """
-1.6.1 The 3n + 1 Problem
+The 3n + 1 Problem
+
+Programming Challenges (Skiena & Revilla): 1.6.1 The 3n + 1 Problem
+
+
+UVa Online Judge: 100 The 3n + 1 Problem 
+Judge Version: PYTH3 3.5.1 - Python 3
+
+Best Time: 1.360 Seconds
 
 """
-"""  """
-def maxCycle(i,j):
-    maxi = 0
-    for x in range(i, j + 1):
-        tot=0
-        while(x > 1):
-            if x % 2 == 0: x /= 2
-            else: x = x * 3 + 1
-            tot += 1
-        if (tot > maxi): maxi = tot + 1
-    return maxi
 
-def pc1():
-    with open('pc161_inputs.txt') as f:
-       for line in f:
-          lst = [int(y) for y in line.replace("\n","").split(' ')]
-          print "{0} {1} {2}".format(lst[0], lst[1], maxCycle(lst[0],lst[1]))
+import sys
+#import functools
 
-if __name__ == "__main__": pc1()
+#Caching can substitute the maxMem dictionary
+#@functools.lru_cache(None)
+def cycleLength(n, maxMem):
+    """
+        Calculates the cycle-legnth of a number
+    
+        Parameters
+        ----------
+        n : int
+            integer to calculate cycle-length for
+        maxMem : dict
+            Maximum Cycle-Length cache passed by reference
+            
+        Returns
+        -------
+        int
+            the cycle-length of the given interval
+    
+    """
+    # Used the stored value in the cache if the was previously calculated
+    if n in maxMem:
+        return maxMem[n]
+    else:
+        orig = n
+        cnt = 1
+    
+        while n != 1:
+            cnt += 1
+            if n%2 ==1:
+                n = 3*n+1
+            else:
+                n = n//2
+        maxMem[orig] = cnt
+        return cnt
+
+#@functools.lru_cache(None)
+def maxCycle(num1, num2, maxMem):
+    """
+        Calculates the maximum cycle between the given range 
+    
+        Calls the cycleLength function for each number 
+    
+        Parameters
+        ----------
+        num1 : int
+            First number in range
+        num2 : int
+            Second number in range
+        maxMem : dict
+            Maximum Cycle-Length cache passed by reference
+            
+        Returns
+        -------
+        int
+            maximum cycle in the range
+    
+    """
+    # The judge may provide a set of numbers that are not in numerical order
+    num1, num2 = min(num1, num2), max(num1, num2)
+    return max(cycleLength(n, maxMem) for n in range(num1, num2+1))
+
+if __name__ == '__main__':
+    # Dictionary used to cache results of previously calculated cycle lengths
+    # Key: integer n
+    # Value: cycle-length for integer n
+    maxMem = {}
+    for line in sys.stdin:
+        #Split input line into two integers: the beginning and end of the range
+        num1, num2 = map(int, line.split()[:2])
+        print(num1, num2, maxCycle(num1, num2, maxMem))
+    exit(0)
+    
+"""
+Minimized
+
+import sys
+def cycleLength(n,maxMem):
+    if n in maxMem:return maxMem[n]
+    else:
+        orig,cnt=n,1
+        while n != 1:
+            cnt+=1
+            if n%2==1:n=3*n+1
+            else:n=n//2
+        maxMem[orig]=cnt
+        return cnt
+def maxCycle(num1,end,maxMem):
+    num1,end = min(num1,end), max(num1,end)
+    return max(cycleLength(n,maxMem) for n in range(num1,end+1))
+if __name__=='__main__':
+    maxMem={}
+    for line in sys.stdin:
+        num1,num2 = map(int,line.split()[:2])
+        print(num1,num2,maxCycle(num1,num2,maxMem))
+    exit(0)
+
+"""
--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # Programming Challenges
 Solutions to problems in the text "Programming Challenges The Programming Contest Training Manual" by Steven S. Skiena and Miguel A. Revilla
 
-Note: The online judges that are used evaluate the solutions' accuracy are currently offline as of this posting. Instead, external text files are supplied with the sample inputs from the text 
+~~Note: The online judges that are used evaluate the solutions' accuracy are currently offline as of this posting. Instead, external text files are supplied with the sample inputs from the text~~
+
+The programs are evaluated using the [UVa Online Judge](https://uva.onlinejudge.org/ "UVa Online Judge Website") 
