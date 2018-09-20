b730bf41432eedf4c502292752af37ab27f24d99
Chris <cjpileggi@gmail.com>
2018-09-19 23:03:57 -0400

    Updated Chap 1 Prob 3

--- Chapter 1/3. The Trip/p163.py
+++ Chapter 1/3. The Trip/p163.py
@@ -1,29 +1,31 @@
-
+from sys import stdin
 def minPrice(prices):
-    minP = 0
+    minP, maxP = 0, 0
     pricesAvg = sum(prices)/len(prices)
     for p in prices:
         if p < pricesAvg:
             #print(p - pricesAvg, float(format(pricesAvg - p, '.3f')[:-1]))
             minP += float(format(pricesAvg - p, '.3f')[:-1])
         
-    return "${}".format(minP) 
-
-def avg(lst):
-    return sum(lst) / len(lst)
+        if p > pricesAvg:
+            #print(p - pricesAvg, float(format(pricesAvg - p, '.3f')[:-1]))
+            maxP += float(format(p - pricesAvg, '.3f')[:-1])
+    
+    return "${:.2f}".format(max(minP,maxP)) 
 
 
 if __name__ == "__main__":
-    flag, priceNum = True, 0
+    flag, priceNum,prices = True, 0, []
     with open('pc163_inputs.txt') as f:
         for line in f: #stdin: 
             if flag:
                 priceNum = int(line)
                 flag=False
             else:
-                prices = [float(p) for p in line.split()]
-                print(prices)
+                prices.append(float(line.replace("\n","")))
                 priceNum -= 1
+                priceNum
                 if priceNum == 0:
                     flag = True
-                print(minPrice(prices))
+                    print(minPrice(prices))
+                    prices = []
