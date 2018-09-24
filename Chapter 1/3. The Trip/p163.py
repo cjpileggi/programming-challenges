c226b0f93235aa8f89878c68d0ae28a44a8d7585
Chris <cjpileggi@gmail.com>
2018-09-23 22:17:13 -0400

    Updated The Trip

--- Chapter 1/3. The Trip/p163.py
+++ Chapter 1/3. The Trip/p163.py
@@ -1,31 +1,28 @@
 from sys import stdin
+from math import ceil,floor
 def minPrice(prices):
     minP, maxP = 0, 0
     pricesAvg = sum(prices)/len(prices)
+    pricesAvgLow = floor(pricesAvg * 100) / 100
+    pricesAvgHigh = ceil(pricesAvg * 100) / 100
     for p in prices:
-        if p < pricesAvg:
-            #print(p - pricesAvg, float(format(pricesAvg - p, '.3f')[:-1]))
-            minP += float(format(pricesAvg - p, '.3f')[:-1])
+        if p < pricesAvg: minP += float(pricesAvgLow - p)
+        if p > pricesAvg: maxP += float(p - pricesAvgHigh)
         
-        if p > pricesAvg:
-            #print(p - pricesAvg, float(format(pricesAvg - p, '.3f')[:-1]))
-            maxP += float(format(p - pricesAvg, '.3f')[:-1])
-    
     return "${:.2f}".format(max(minP,maxP)) 
 
 
 if __name__ == "__main__":
     flag, priceNum,prices = True, 0, []
-    with open('pc163_inputs.txt') as f:
-        for line in f: #stdin: 
-            if flag:
-                priceNum = int(line)
-                flag=False
-            else:
-                prices.append(float(line.replace("\n","")))
-                priceNum -= 1
-                priceNum
-                if priceNum == 0:
-                    flag = True
-                    print(minPrice(prices))
-                    prices = []
+    #with open('pc163_inputs.txt') as f:
+    for line in stdin: 
+        if flag:
+            priceNum = int(line)
+            flag=False
+        else:
+            prices.append(float(line))
+            priceNum -= 1
+            if priceNum == 0:
+                flag = True
+                print(minPrice(prices))
+                prices = []
