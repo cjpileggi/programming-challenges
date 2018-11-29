9ed3532c907ab242815ab474f3c9b96234b81768
Chris <cjpileggi@gmail.com>
2018-11-28 22:48:20 -0500

    Problem 1: Changed map to unordered_map in C++

--- Chapter 1/1. The 3n+1 Problem/p1_1.cpp
+++ Chapter 1/1. The 3n+1 Problem/p1_1.cpp
@@ -2,11 +2,108 @@
     #define ONLINE_JUDGE false
 #endif
 
-#include <map>
+#include <unordered_map>
+#include <iostream>
 
-using std::map;
 
-unsigned int cycleLength(unsigned int n, map<int, int> maxMem)
+using std::unordered_map;
+
+unsigned int cycleLength(unsigned int n, unordered_map<int, int> &maxMem)
+{
+	unsigned int orig = n, cnt = 1;
+
+	while (n != 1) {
+		cnt++;
+		if (n%2 == 1) {n = 3*n+1;}
+		else {n /= 2;}
+		
+		if (maxMem.find(n) != maxMem.end())
+		{
+			cnt = maxMem[n] + (cnt - 1);
+			break;
+		}
+	}
+	maxMem[orig] = cnt;
+	
+	return cnt;
+}
+
+
+unsigned int maxCycle(unsigned int i, unsigned int j, unordered_map<int, int> &maxMem) {
+    if (i > j)
+	{
+		unsigned int temp;
+		temp = i;
+		i = j;
+		j = temp;
+	}
+	
+	unsigned int maxCnt = 0, curCycle;
+	for (unsigned int c = i; c <= j; c++)
+	{
+		curCycle = 0;
+		if (maxMem.find(c) != maxMem.end())
+		{
+			curCycle = maxMem[c];
+		}
+		else
+		{
+			curCycle = cycleLength(c, maxMem);
+		}
+		
+		
+		if (curCycle >  maxCnt)
+		{
+			maxCnt = curCycle;
+		}
+	}
+	
+	return maxCnt;
+} 
+
+int main()
+{	
+	unsigned int i, j;
+	unordered_map<int, int> maxMem;
+	
+	//FILE *in = fopen("./text.txt", "r");
+	//while(fscanf(in, "%d %d", &i, &j) != EOF ) {
+	while(scanf("%d %d", &i, &j) != EOF ) {
+		
+		printf("%d %d %d\n", i, j, maxCycle(i, j, maxMem)); 
+	} 
+	// Create a map iterator and point to beginning of map
+	/*std::map<int, int>::iterator it = maxMem.begin();
+ 
+	// Iterate over the map using Iterator till end.
+	while (it != maxMem.end())
+	{
+		// Accessing KEY from element pointed by it.
+		int word = it->first;
+ 
+		// Accessing VALUE from element pointed by it.
+		int count = it->second;
+ 
+		std::cout << word << " :: " << count << std::endl;
+ 
+		// Increment the Iterator to point to next entry
+		it++;
+	} */
+    return 0;
+} 
+
+/*
+#ifndef ONLINE_JUDGE
+    #define ONLINE_JUDGE false
+#endif
+
+#include <unordered_map>
+//#include <iostream>
+
+
+using std::unordered_map;
+
+unsigned int cycleLength(unsigned int n, unordered_map<int, int> &maxMem)
 {
     if (maxMem.find(n) != maxMem.end()) {
         return maxMem[n];
@@ -21,12 +118,13 @@ unsigned int cycleLength(unsigned int n, map<int, int> maxMem)
             else {n = n/2;}
 		}
         maxMem[orig] = cnt;
+		
         return cnt;
 	}
 }
 
 
-unsigned int maxCycle(unsigned int i, unsigned int j, map<int, int> maxMem) {
+unsigned int maxCycle(unsigned int i, unsigned int j, unordered_map<int, int> &maxMem) {
     if (i > j)
 	{
 		unsigned int temp;
@@ -52,13 +150,30 @@ unsigned int maxCycle(unsigned int i, unsigned int j, map<int, int> maxMem) {
 int main()
 {	
 	unsigned int i, j;
-	map<int, int> maxMem;
+	unordered_map<int, int> maxMem;
 	
 	//FILE *in = fopen("./text.txt", "r");
 	//while(fscanf(in, "%d %d", &i, &j) != EOF ) {
 	while(scanf("%d %d", &i, &j) != EOF ) {
 		
 		printf("%d %d %d\n", i, j, maxCycle(i, j, maxMem)); 
-	}
+	} 
+	// Create a map iterator and point to beginning of map
+	/*std::map<int, int>::iterator it = maxMem.begin();
+ 
+	// Iterate over the map using Iterator till end.
+	while (it != maxMem.end())
+	{
+		// Accessing KEY from element pointed by it.
+		int word = it->first;
+ 
+		// Accessing VALUE from element pointed by it.
+		int count = it->second;
+ 
+		std::cout << word << " :: " << count << std::endl;
+ 
+		// Increment the Iterator to point to next entry
+		it++;
+	} 
     return 0;
-}
+}*/
