756a75bc9116039d7afd892232028fecb9f3167e
Chris <cjpileggi@gmail.com>
2018-12-22 22:53:16 -0500

    Problem 5 C++ - Updated

--- Chapter 1/5. Graphical Editor/pc165.cpp
+++ Chapter 1/5. Graphical Editor/pc165.cpp
@@ -2,51 +2,58 @@
     #define ONLINE_JUDGE false
 #endif
 
-class image {
+#include <string>
 
-	void command(char[] cmd)
-	{
-		switch (cmd[0]) {
-			case "I": 
-				newImgI();
-				break;
-			case "C":
-				clearC();
-				break;
-			case "L":
-				locationL();
-				break;
-			case "V":
-				vertV();
-				break;
-			case "H":
-				horizH();
-				break;
-			case "K":
-				rectK();
-				break;
-			case "F":
-				fillF();
-				break;
-			case "S":
-				saveS();
-				break;
-			default:
-				break;
-		}
-	}
+using std::string;
+
+class Image {
+
+	public:
+    void command(string[] cmd)
+    	{
+    		switch (cmd[0]) {
+    			case 'I':
+    				newImgI();
+    				break;
+    			case 'C':
+    				clearC();
+    				break;
+    			case 'L':
+    				locationL();
+    				break;
+    			case 'V':
+    				vertV();
+    				break;
+    			case 'H':
+    				horizH();
+    				break;
+    			case 'K':
+    				rectK();
+    				break;
+    			case 'F':
+    				fillF();
+    				break;
+    			case 'S':
+    				saveS();
+    				break;
+    			default:
+    				break;
+    		}
+    	}
+
 };
 
-int main()
-{	
 
+int main()
+{
+ /*
 	//FILE *in = fopen("./text.txt", "r");
 	//while(fscanf(in, "%d %d", &i, &j) != EOF ) {
 	while(scanf("%d %d", &i, &j) != EOF ) {
-		
-		printf("%d %d %d\n", i, j, maxCycle(i, j, maxMem)); 
-	} 
 
+		printf("%d %d %d\n", i, j, maxCycle(i, j, maxMem));
+	}
 
+*/
     return 0;
 }
