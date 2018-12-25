dde5a5089e166522e5cdc538838f85123637e046
Chris <cjpileggi@gmail.com>
2018-12-25 08:55:34 -0500

    Problem 1: C++ - Add More Functions

--- Chapter 1/5. Graphical Editor/pc165.cpp
+++ Chapter 1/5. Graphical Editor/pc165.cpp
@@ -3,28 +3,32 @@
 #endif
 
 #include <string>
+#include <algorithm>
 
 using std::string;
+using std::min;
+using std::max;
 
 class Image {
 
 	public:
-    unsigned int **img;
+    char **img;
+    int rows, cols;
 
-    void command(string[] cmd)
+    void command(char* cmd)
     	{
     		switch (cmd[0]) {
     			case 'I':
-    				newImgI();
+    				newImgI(cmd[1], cmd[2]);
     				break;
     			case 'C':
     				clearC();
     				break;
     			case 'L':
-    				locationL();
+    				locationL(cmd[1], cmd[2], cmd[3]);
     				break;
     			case 'V':
-    				vertV();
+    				vertV(cmd[1], cmd[2], cmd[3], cmd[4]);
     				break;
     			case 'H':
     				horizH();
@@ -47,12 +51,46 @@ class Image {
       {
             //return [["0"] * int(cmd[1])] * int(cmd[2])
             //creates duplicates of same list(all elements in column affected)
-            img = new unsigned int*[N];
-            for(int i = 0; i < N; i++)
+
+            img = new char*[M];
+            for(int i = 0; i < M; i++)
             {
-                img[i] = new unsigned int[M];
+              img[i] = new char[N];
               //self.img.append(["O" for x in range(0,M)])
             }
+
+            for(int i=0; i < M; i++)
+            {
+              for(int j=0; j < N; j++)
+              {
+                img[i][j] = '0';
+              }
+            }
+      }
+
+      void clearC()
+      {
+        for(int i=0; i < rows; i++)
+        {
+          for(int j=0; j < cols; j++)
+          {
+            img[i][j] = '0';
+          }
+        }
+      }
+
+      void locationL(unsigned int X, unsigned int Y, char C)
+      {
+          img[Y - 1][X - 1] = C;
+      }
+
+      void vertV(unsigned int X, unsigned int Y1, unsigned int Y2, char C)
+      {
+
+          for(int y=(min(Y2,Y1) - 1 ); y < max(Y1,Y2); y++)
+          {
+              img[y][X-1] = C;
+          }
       }
 
 };
